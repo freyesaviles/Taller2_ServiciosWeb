26fc72db1a46a43d0e3b7e38ce921442fadb5388
freyesaviles <freyesaviles@uamv.edu.ni>
2026-09-14 16:13:32 -0600

    Initial commit

---
+++ src/main/java/com/uam/taller2_serviciosweb/Taller2ServiciosWebApplication.java
@@ -0,0 +1,13 @@
+package com.uam.taller2_serviciosweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2ServiciosWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller2ServiciosWebApplication.class, args);
+    }
+
+}
